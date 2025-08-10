354018207acaa5a691a02e518151b5dc7a20fcaf
jigarnayak1290 <jigarnayak1290@gmail.com>
2025-08-10 15:51:28 +0900

    Required file added

---
+++ MerkleTree/Models/MerkleTreeNode.cs
@@ -0,0 +1,10 @@
+﻿namespace MerkleTree.Models
+{
+    /// <summary>
+    /// Single node information of Merkle tree (As Type Record for immutability)
+    /// </summary>
+    public record MerkleTreeNode(
+        string Hash, 
+        MerkleTreeNode? Left = null, 
+        MerkleTreeNode? Right = null);
+}
