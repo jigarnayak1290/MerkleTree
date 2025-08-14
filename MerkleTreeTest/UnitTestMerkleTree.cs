e62e1d31b09d764c9ac3f66ea1e614eb4089a382
jigarnayak1290 <jigarnayak1290@gmail.com>
2025-08-14 09:47:50 +0900

    Code improved & test project added

---
+++ MerkleTreeTest/UnitTestMerkleTree.cs
@@ -0,0 +1,24 @@
+using MerkleTree;
+
+namespace MerkleTreeTest
+{
+    public class UnitTestMerkleTree
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange test data (Example transactions to be hashed)
+            // In a real-world scenario, these would be actual transaction data.
+            List<string> transactions = new List<string>
+            { "aaa", "bbb", "ccc", "ddd", "eee" };
+
+            // Act (calculate the Merkle root)
+            var service = new MerkleTreeService();
+            var result = service.CalculateMerkleRoot(transactions);
+
+            // Assert (Checking the root hash with result)
+            var rootHash = result.Hash;
+            Assert.Equal("4aa906745f72053498ecc74f79813370a4fe04f85e09421df2d5ef760dfa94b5", rootHash);
+        }
+    }
+}
